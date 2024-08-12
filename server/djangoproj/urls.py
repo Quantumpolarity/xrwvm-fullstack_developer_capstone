51f4724673f7b1130663d3dc95e99dafe680bb54
Quantumpolarity <168380651+Quantumpolarity@users.noreply.github.com>
2024-08-12 08:00:05 -0700

    Update lint errorsurls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     ),
     path(
         route='dealer/<int:dealer_id>',
-        view=views.get_dealer_details, 
+        view=views.get_dealer_details,
         name='dealer_details',
     ),    
     # path for dealer reviews view
