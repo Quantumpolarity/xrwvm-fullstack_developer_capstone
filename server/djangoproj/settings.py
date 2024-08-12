c7870d0d675aae79e5fbcd5c859ed6a3dfe2a0ef
Quantumpolarity <168380651+Quantumpolarity@users.noreply.github.com>
2024-08-12 12:11:57 -0700

    Update lint errors settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = [
     'localhost',
     'https://fbroussard-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.'
     'cognitiveclass.ai',
-    'https://fbroussard-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.'
-    'cognitiveclass.ai',
+    'https://fbroussard-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+    'proxy.cognitiveclass.ai',
     ]
 CSRF_TRUSTED_ORIGINS = [
     'https://fbroussard-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.'
