5ca393c418485b16421feec9be4645284d87808b
Quantumpolarity <168380651+Quantumpolarity@users.noreply.github.com>
2024-08-19 08:00:59 -0700

    Update lint errors populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -48,15 +48,15 @@ car_make_instances.append(car_make_instance)
       {"name": "Sorrento", "type": "SUV", "year": 2023,
        "car_make": car_make_instances[3]},
       {"name": "Carnival", "type": "SUV", "year": 2023,
-       "car_make":car_make_instances[3]},
+       "car_make": car_make_instances[3]},
       {"name": "Cerato", "type": "Sedan", "year": 2023,
        "car_make":car_make_instances[3]},
       {"name": "Corolla", "type": "Sedan", "year": 2023,
-       "car_make":car_make_instances[4]},
+       "car_make": car_make_instances[4]},
       {"name": "Camry", "type": "Sedan", "year": 2023,
-       "car_make":car_make_instances[4]},
+       "car_make": car_make_instances[4]},
       {"name": "Kluger", "type": "SUV", "year": 2023,
-       "car_make":car_make_instances[4]},
+       "car_make": car_make_instances[4]},
         # Add more CarModel instances as needed
     ]
 
