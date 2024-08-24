c2654e1a735d2de08a70d71b03e046cf065951e1
Fred <f.d.broussard@gmail.com>
2024-08-24 17:03:13 -0400

    Containerize application

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,44 +1,17 @@
 # Uncomment the imports before you add the code
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-    path(route='register', view=views.registration, name='register'),
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-
-    # path for logout
-    path(route='logout', view=views.logout_request, name='logout'),
-
-    # path for get cars
-    path(route='get_cars', view=views.get_cars, name='getcars'),
-
-    # path for get dealers
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(
-        route='get_dealers/<str:state>',
-        view=views.get_dealerships,
-        name='get_dealers_by_state',
-    ),
-    path(
-        route='dealer/<int:dealer_id>',
-        view=views.get_dealer_details,
-        name='dealer_details',
-    ),
-    # path for dealer reviews view
-    path(
-        route='reviews/dealer/<int:dealer_id>',
-        view=views.get_dealer_reviews,
-        name='dealer_details',
-    ),
-    # path for add a review view
-    path(
-        route='add_review',
-        view=views.add_review,
-        name='add_review',
-    ),
+     path('admin/', admin.site.urls),
+    path('djangoapp/', include('djangoapp.urls')),
+    path('', TemplateView.as_view(template_name="Home.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
